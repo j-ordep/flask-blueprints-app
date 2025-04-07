d28326af22841388446d3162bee1fd80fa873cd2
j-ordep <joaoppinheiro2005@email.com>
2025-04-07 16:30:00 -0300

    feat: test database (memory)

---
+++ database/client.py
@@ -0,0 +1,7 @@
+CLIENTS = [
+    {"id":1, "name": "Guilherme", "email": "guilherme@gmail.com"},
+    {"id":2, "name": "Gabriel", "email": "gabriel@gmail.com"},
+    {"id":3, "name": "Roberta", "email": "roberta@gmail.com"},
+    {"id":4, "name": "João", "email": "joao@gmail.com"},
+    {"id":5, "name": "Juliana", "email": "juliana@gmail.com"}
+]
